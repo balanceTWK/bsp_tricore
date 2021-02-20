69405740f95b22cc58814b859f18a31b4983963a
tangweikang <tangweikang@rt-thread.com>
2021-02-20 15:30:54 +0800

    [TC364] rt-thread startup ok.

--- TC364/USER/Cpu0_Main.c
+++ TC364/USER/Cpu0_Main.c
@@ -8,23 +8,64 @@ uint16 num;
 
 int core0_main(void)
 {
-	get_clk();
-	enableInterrupts();
+extern int rtthread_startup(void);
 
-	IfxCpu_emitEvent(&g_cpuSyncEvent);
-	IfxCpu_waitEvent(&g_cpuSyncEvent, 0xFFFF);
-	enableInterrupts();
-	while (TRUE)
-	{
-		num++;
-    	printf("printf num :%d\r\n", num);
-    	systick_delay_ms(STM0, 1000);
-	}
+    disableInterrupts();
+	get_clk();
+
+    rtthread_startup();
+
+    rt_kprintf("Failed to start rt-thread.\n");
+
+    while(1);
+
 }
+
+void GPIO_Demo_init(void)
+{
+    /* configure P20.8 as general output */
+    IfxPort_setPinMode(&MODULE_P20, 8,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P20.9 as general output */
+    IfxPort_setPinMode(&MODULE_P20, 9,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P21.4 as general output */
+    IfxPort_setPinMode(&MODULE_P21, 4,  IfxPort_Mode_outputPushPullGeneral);
+    /* configure P21.5 as general output */
+    IfxPort_setPinMode(&MODULE_P21, 5,  IfxPort_Mode_outputPushPullGeneral);
+}
+
+static rt_uint8_t led_thread_stack[2048];
+struct rt_thread led_thread_thread;
+void led_thread_entry(void *parameter)
+{
+    while(1)
+    {
+        IfxPort_togglePin(&MODULE_P20, 8);
+        IfxPort_togglePin(&MODULE_P20, 9);
+        rt_thread_mdelay(1000);
+        IfxPort_togglePin(&MODULE_P21, 4);
+        IfxPort_togglePin(&MODULE_P21, 5);
+    }
+}
+
 
 int main(void)
 {
-    while(1);
+    rt_thread_t tid;
+    rt_err_t result;
+
+    rt_uint32_t count;
+
+    GPIO_Demo_init();
+    tid = &led_thread_thread;
+    result = rt_thread_init(tid, "led", led_thread_entry, RT_NULL,
+                led_thread_stack, sizeof(led_thread_stack), 10, 20);
+    RT_ASSERT(result == RT_EOK);
+    rt_thread_startup(tid);
+    while(1)
+    {
+        rt_thread_mdelay(1000);
+        rt_kprintf("hello rt-thread! %d\n",count++);
+    }
 }
 
 
